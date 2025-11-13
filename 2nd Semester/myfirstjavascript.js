66e7a2f2e9241b1a8bc7b782e9c9797e714b99e8
jaereeeee <jerelineannrosa@gmail.com>
2025-11-14 07:21:25 +0800

    asdfgh

---
+++ 2nd Semester/myfirstjavascript.js
@@ -0,0 +1,6 @@
+    function changeP3() {
+        document.getElementById('p3').innerHTML = 'Paragraph 3 changed by JavaScript in the body'
+    }
+    function Alert3(){
+    alert("this is an alert 3 from JavaScript from the html")
+    }
